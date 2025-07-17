09f9fe9e88c45e1397e33fefbbd643eccc123a68
Szymon Łanucha <slann@protonmail.com>
2025-07-17 16:53:22 +0200

    Create flake.nix
    Initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = inputs: {
+    nixosConfigurations.nixos = inputs.lib.nixosSystem {
+      modules = [
+        { nix.settings.experimental-features = ["nix-command" "flakes"]; }
+        ./configuration.nix
+      ];
+    };
+  }; 
+}
